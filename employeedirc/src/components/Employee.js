8fdfc6ac39e2f914171cb841004d3587dfbf6c6b
Gina Vargas <gina.vargas89@outlook.com>
2021-04-07 18:42:42 -0700

    sudo code

---
+++ employeedirc/src/components/Employee.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+//create a table for employees 
+// employee data should include first name, last name, DOB, phone number
+function EmployeeDirc() {
+    const table = (props) => {
+        return (
+            <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+        )
+    }
+ }
+
+
+
+// filter employee data by name
